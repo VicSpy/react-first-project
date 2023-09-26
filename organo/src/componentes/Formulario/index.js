afd76dc38ad21f4ecbb96b1b796e2fecd081d32e
Victor Kruger <98160719+VicSpy@users.noreply.github.com>
2023-09-25 21:20:26 -0300

    New components

---
+++ organo/src/componentes/Formulario/index.js
@@ -0,0 +1,30 @@
+import './Formulario.css'
+import CampoTexto from "../CampoTexto"
+import ListaSuspensa from '../ListaSuspensa'
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    return (
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colabordador</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa itens={times} label="Time"/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
